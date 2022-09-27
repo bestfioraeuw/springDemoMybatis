bd2cc3baeddacbdc082a5cb176d29da92ebcc887
bestfioraeuw <651745708@qq.com>
2022-09-28 00:14:06 +0800

    mybatis整合和lombok生成方法

---
+++ springCRUDdemo/src/test/java/com/lagou/springcruddemo/SpringCruDdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagou.springcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCruDdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
